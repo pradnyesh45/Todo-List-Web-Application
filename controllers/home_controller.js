374fb7b1cc8946e14121e04be1022c57ae528139
Pradnyesh Aglawe <pradnyeshaglawe12@gmail.com>
2020-05-04 19:44:47 +0530

    added styling to the page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,7 @@ const Task = require('../models/task');
 module.exports.home = function(req, res){
     Task.find({}, function(err, tasks){
         if (err){
-            console.log('Error in fetching contacts from db');
+            console.log('Error in fetching tasks from db');
             return;
         }
 
