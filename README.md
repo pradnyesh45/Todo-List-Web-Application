8a7fca257c1e5a11a14697a723761bea09e0d6b7
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-02-08 10:34:46 +0530

    Added screenshot

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Todo-List-Web-Application
+
 A Todo List WebApp with Node.js, Express.js and MongoDB
 <br>
+
+# Screenshot
+
+![Screenshot](assets/images/todo_list.jpg?raw=true "Screenshot")
+
+<br>
 To run this project in your local computer follow the following steps:<br>
+
 1. Install Node.js and npm.
 2. Install nodemon using command $ npm install -g nodemon
 3. Clone the project and open the terminal with project directory.
--- index.js
+++ index.js
@@ -1,58 +1,61 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/mongoose');
-const Task = require('./models/task');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const db = require("./config/mongoose");
+const Task = require("./models/task");
 
 const app = express();
 const port = 8000;
 
 // use express router
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
+
+const jsonParser = bodyParser.json();
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // setting up view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
-app.use(express.urlencoded());
-app.use(express.static('./assets'));
-
-
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("./assets"));
 
 // creating a Task
-app.post('/create-task', function(req, res){
-    Task.create({
-        task: req.body.task
-    }, function(err, newTask){
-        if (err){
-            console.log('Error in creating a Task!'); 
-            return;
-        }
+app.post("/create-task", jsonParser, function (req, res) {
+  Task.create(
+    {
+      task: req.body.task,
+    },
+    function (err, newTask) {
+      if (err) {
+        console.log("Error in creating a Task!");
+        return;
+      }
 
-        console.log('******', newTask);
-        return res.redirect('back');
-    });
+      console.log("******", newTask);
+      return res.redirect("back");
+    }
+  );
 });
 
 // Deleting a task
-app.get('/delete-task', function(req, res){
-    let id = req.query.id;
+app.get("/delete-task", jsonParser, function (req, res) {
+  let id = req.query.id;
 
-    Task.findByIdAndDelete(id, function(err){
-        if (err){
-            console.log('error in deleting an object from database');
-            return;
-        }
-
-        return res.redirect('back');
-
-    });
-
-});
-
-// listening to the port 
-app.listen(port, function(err){
-    if (err){
-        console.log(`Error in running the server: ${port}`);
+  Task.findByIdAndDelete(id, function (err) {
+    if (err) {
+      console.log("error in deleting an object from database");
+      return;
     }
 
-    console.log(`Server is running on port: ${port}`);
+    return res.redirect("back");
+  });
+});
+
+// listening to the port
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${port}`);
+  }
+
+  console.log(`Server is running on port: ${port}`);
 });
